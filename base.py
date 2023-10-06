c489d678dad2a74a4e64862d6c7b835c157ad71d
cabrerajesus <jesus.cabrera@capital-minds.com>
2023-10-06 15:40:43 -0400

    Desarrollo de codigo base

---
+++ base.py
@@ -0,0 +1,9 @@
+# Solicitar la base y la potencia al usuario
+base = float(input("Ingresa la base: "))
+potencia = int(input("Ingresa la potencia: "))
+
+# Calcular la potencia
+resultado = base ** potencia
+
+# Mostrar el resultado
+print(f"{base} elevado a la potencia {potencia} es igual a {resultado}")
